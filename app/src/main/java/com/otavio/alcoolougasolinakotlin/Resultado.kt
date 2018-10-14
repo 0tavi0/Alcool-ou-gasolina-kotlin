4b49a118ed94ad829754f09f069fcc7f5531e831
Otavio <otavio.le@gmail.com>
2018-10-14 20:14:15 -0300

    first commit

---
+++ app/src/main/java/com/otavio/alcoolougasolinakotlin/Resultado.kt
@@ -0,0 +1,18 @@
+package com.otavio.alcoolougasolinakotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_resultado.*
+
+class Resultado : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        val ss:String = intent.getStringExtra("resultado")
+
+        txtResultado.setText(ss)
+
+    }
+}
